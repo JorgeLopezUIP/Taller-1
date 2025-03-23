bf7c28a7c99dc38e8d597ca187a945a9f3594e82
JorgeLopezUIP <157850804+JorgeLopezUIP@users.noreply.github.com>
2025-03-22 23:13:06 -0500

    Add files via upload

---
+++ import sqlite3.py
@@ -0,0 +1,14 @@
+import sqlite3 
+
+e = True
+
+class Libro: 
+
+    def __init__(self, titulo, genero, autor, estado): 
+        self.titulo = titulo 
+        self.genero = genero 
+        self.autor = autor 
+        self.estado = estado 
+
+while e: 
+            
